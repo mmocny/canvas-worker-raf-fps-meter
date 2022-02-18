165065aea76a2bd4f8f1d65a324f3e763e6cefed
Michal Mocny <mmocny@gmail.com>
2022-02-18 15:36:49 -0500

    Add a simple comment from github vscode editor

--- EstimateDeviceFps.mjs
+++ EstimateDeviceFps.mjs
@@ -6,6 +6,9 @@ import * as Comlink from "https://unpkg.com/comlink/dist/esm/comlink.mjs";
 const frameToFrameTime = Comlink.wrap(
 	new Worker(new URL("./EstimateDeviceFpsWorker.mjs", import.meta.url).href, { type: "module" }));
 
+// Estimate FPS by way of using an WebWorker + double-rAF
+// Unlike double-rAF on main thread, which can get delayed by long tasks and lack of rendering opportunities,
+//   double-rAF in a worker will get synchronized to the device refresh rate with high probability
 export async function estimateDeviceFps() {
 	const f2f = await frameToFrameTime();
 	const fps = 1000 / f2f;
